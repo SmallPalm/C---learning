61c2eedd6a82fb6b33a1c46141ce0b3172a2c045
coderLi <17731035485@163.com>
2024-12-18 18:33:04 +0800

    feat: 使用 for 循环输出 1 到 10 的所有偶数

---
+++ 01-Init-C++_project/evenNunber.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int evenNum; 
+
+  for(int i = 1; i <= 10; i++) {
+    if( i % 2) {
+      cout << i << endl;
+    }
+  }
+
+  return 0;
+}
