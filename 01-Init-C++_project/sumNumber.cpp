8276dbf2eda1042ff34954e05f3954f259af2dbf
coderLi <17731035485@163.com>
2024-12-18 18:35:47 +0800

    feat: 写一个 while 循环，计算 1 到 100 的累加和

---
+++ 01-Init-C++_project/sumNumber.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+  int sum;
+  int count = 0;
+  while(count =< 100) {
+    count++;
+    sum += count;
+  }
+
+  cout << sum << endl;
+  return 0
+}
